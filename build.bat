6ba7c406d8f289e0278d7120a57b77ae122c6dd0
jewkub <jew.napat@hotmail.com>
2023-11-08 17:56:46 +0700

    🍋 Prototype 1 - Select scanned and renamed folder - Watch scanned folder -> move scanned file to this folder - Read barcode - Rename file - Move to renamed folder

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller main.spec --noconfirm
+cd dist/main/
+mkdir scanned
+mkdir renamed
+main.exe
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python3
 import sys
+import shutil
+import os
 from pyzbar import pyzbar
 from pathlib import Path
 import cv2
 import tkinter
 from tkinter import filedialog
-
-# https://stackoverflow.com/a/43046744
-# from ctypes import windll
-# windll.shcore.SetProcessDpiAwareness(1)
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 root = tkinter.Tk()
 root.withdraw() # use to hide tkinter window
 
-def get_path():
+def get_path(prompt):
   currdir = Path.cwd()
-  tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
+  tempdir = filedialog.askdirectory(
+    parent=root,
+    initialdir=currdir,
+    title='Select a directory for ' + prompt + ' files',
+  )
   if len(tempdir) > 0:
     print ("You chose: %s" % tempdir)
   return tempdir
@@ -31,7 +38,7 @@ def decode(image):
     print("Data:", obj.data)
     print()
 
-  return image
+  return (image, decoded_objects)
 
 def draw_barcode(decoded, image):
   # n_points = len(decoded.polygon)
@@ -44,21 +51,56 @@ def draw_barcode(decoded, image):
     thickness=5)
   return image
 
+class ScannedFileHandler(FileSystemEventHandler):
+  def on_created(self, event):
+    # dir_path = event.src_path.split('/input_files')
+    # processed_files = f'{dir_path[0]}/processed_files'
+    # child_processed_dir = create_directory(file_path=processed_files)
+    if event:
+      objs = None
+      print("file created:{}".format(event.src_path))
+      if not event.src_path:
+        print("Invalid file path")
+        return
+      file_path = Path(event.src_path).as_posix()
+      old_size = -1
+      while (old_size != os.path.getsize(file_path)):
+        old_size = os.path.getsize(file_path)
+        time.sleep(0.1)
+      img = cv2.imread(file_path)
+      (img, objs) = decode(img)
+      # cv2.imshow("img", img)
+      # cv2.waitKey(0)
+
+      if type(objs) is list and objs[0] is not None:
+        file_name = objs[0].data.decode("utf-8") + Path(file_path).suffix
+        dest_path = Path(renamed_path) / file_name
+        # print(dest_path)
+        shutil.move(file_path, dest_path)
+        print("file moved:{} to {}".format(file_path, dest_path))
+        cv2.destroyAllWindows()
+
 if __name__ == "__main__":
-  print(sys.executable)
-  DIRNAME = Path(__file__).parent.resolve()
-  print(DIRNAME.as_posix())
-  path = get_path()
-  if len(path) == 0:
-    exit()
-  barcodes = DIRNAME.glob("barcode*.png")
-  for barcode_file in barcodes:
-    # load the image to opencv
-    print(barcode_file.as_posix())
-    img = cv2.imread(barcode_file.as_posix())
-    # decode detected barcodes & get the image
-    # that is drawn
-    img = decode(img)
-    # show the image
-    cv2.imshow("img", img)
-    cv2.waitKey(0)
+  # print(Path.cwd())
+  # DIRNAME = Path(__file__).parent.resolve()
+  # print(DIRNAME.as_posix())
+  scanned_path = get_path('scanned')
+  if len(scanned_path) == 0:
+    sys.exit(0)
+  renamed_path = get_path('renamed')
+  if len(renamed_path) == 0:
+    sys.exit(0)
+
+  logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+  observer = Observer()
+  observer.schedule(ScannedFileHandler(), scanned_path, recursive=True)
+  observer.start()
+
+  try:
+    while True:
+      time.sleep(1000)
+  except KeyboardInterrupt:
+    observer.stop()
+  observer.join()
