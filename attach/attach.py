17c0bdd0b912df81e493cd46ad173f1b405b48ab
jewkub <jew.napat@hotmail.com>
2024-03-18 16:28:17 +0700

    rename

--- attach/attach.py
+++ attach/attach.py
@@ -5,29 +5,32 @@ from pathlib import Path
 import tkinter
 from tkinter import messagebox
 import pyautogui
+import re
 
-def withsleep (fn, sleeptime=0.6):
+def withsleep (fn, sleeptime=1.0):
   time.sleep(sleeptime)
 
 if __name__ == "__main__":
   root = tkinter.Tk()
   root.withdraw() # use to hide tkinter window
   messagebox.showinfo("Ready", "หลังจากกด OK แล้วกดไปที่ HCLAB และกดคลิกช่องใส่ Surgical No. ทันที")
-  time.sleep(4)
+  time.sleep(10)
 
   p = (Path(__file__).parent / 'renamed').glob('*')
   files = [x for x in p if x.is_file()]
-  print(files)
+  # print(files)
   for file in files:
-    for surgicalNo in file.stem.split('-')[0].split(','):
+    # print(re.split('-| ', file.stem)[0].split(','))
+    for surgicalNo in re.split('-| ', file.stem)[0].split(','):
+      print(surgicalNo)
       surgicalNo = surgicalNo.strip()
       withsleep(pyautogui.write(surgicalNo), 0.2)
       withsleep(pyautogui.press('enter'))
       withsleep(pyautogui.hotkey('ctrl', 'pagedown'), 0.2)
       withsleep(pyautogui.press('enter'))
-      withsleep(pyautogui.write(file.stem))
-      withsleep(pyautogui.press('down'))
-      withsleep(pyautogui.press('enter'))
+      withsleep(pyautogui.write(file.stem), 0.6)
+      withsleep(pyautogui.press('down'), 0.6)
+      withsleep(pyautogui.press('enter'), 2.0)
       withsleep(pyautogui.hotkey('ctrl', 'pagedown'), 0.2)
       withsleep(pyautogui.press('tab', presses=3), 0.2)
       withsleep(pyautogui.press('enter'))
